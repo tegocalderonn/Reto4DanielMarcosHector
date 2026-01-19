727c506dadec947f3d238e91c09c21699ba0d7db
abenia26 <1DAM@A11-PC27>
2026-01-19 13:08:07 +0100

    yhyhh

---
+++ Reto4DanielMarcosHector/src/clases/Catamaran.java
@@ -0,0 +1,25 @@
+package clases;
+import clases.Motor;
+public class Catamaran extends Barco {
+protected Motor motorSecundario;
+
+public Catamaran(String nombre, Motor motor, int capacidadCombustible, Motor motorSecundario) {
+	super(nombre, motor, capacidadCombustible);
+	this.motorSecundario = motorSecundario;
+}
+
+public Motor getMotorSecundario() {
+	return motorSecundario;
+}
+
+public void setMotorSecundario(Motor motorSecundario) {
+	this.motorSecundario = motorSecundario;
+}
+
+@Override
+public String toString() {
+	return "Catamaran [motorSecundario=" + motorSecundario + ", nombre=" + nombre + ", motor=" + motor
+			+ ", capacidadCombustible=" + capacidadCombustible + "]";
+}
+
+}
