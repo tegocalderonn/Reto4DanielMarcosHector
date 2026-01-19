c033b0365123a90002101e78ff2ce98935884680
abenia26 <1DAM@A11-PC27>
2026-01-19 12:47:22 +0100

    ddd

--- Reto4DanielMarcosHector/src/clases/Motor.java
+++ Reto4DanielMarcosHector/src/clases/Motor.java
@@ -1,5 +1,36 @@
 package clases;
 
 public class Motor {
+protected String modelo;
+protected int potencia;
+protected double consumo;
+public Motor(String modelo, int potencia, double consumo) {
+	super();
+	this.modelo = modelo;
+	this.potencia = potencia;
+	this.consumo = consumo;
+}
+public String getModelo() {
+	return modelo;
+}
+public void setModelo(String modelo) {
+	this.modelo = modelo;
+}
+public int getPotencia() {
+	return potencia;
+}
+public void setPotencia(int potencia) {
+	this.potencia = potencia;
+}
+public double getConsumo() {
+	return consumo;
+}
+public void setConsumo(double consumo) {
+	this.consumo = consumo;
+}
+@Override
+public String toString() {
+	return "Motor [modelo=" + modelo + ", potencia=" + potencia + ", consumo=" + consumo + "]";
+}
 
 }
