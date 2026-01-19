81acac7d3fb6167d5e6075bef5bf958878eff330
tegocalderonn <1DAM@A11-PC19>
2026-01-19 13:09:38 +0100

    Merge branch 'main' of https://github.com/tegocalderonn/Reto4DanielMarcosHector.git into main

---
+++ Reto4DanielMarcosHector/src/clases/Lancha.java
@@ -0,0 +1,32 @@
+package clases;
+
+public class Lancha extends Barco {
+
+	//Creación variables.
+	protected boolean cubierta;
+
+	public Lancha(String nombre, Motor motor, int capacidadCombustible, boolean cubierta) {
+		super(nombre, motor, capacidadCombustible);
+		this.cubierta = cubierta;
+	}
+
+	public boolean isCubierta() {
+		return cubierta;
+	}
+
+	public void setCubierta(boolean cubierta) {
+		this.cubierta = cubierta;
+	}
+
+	@Override
+	public String toString() {
+		return "Lancha [cubierta=" + cubierta + ", toString()=" + super.toString() + "]";
+	}
+	
+	public double calcularAutonomia() {
+		return super.calcularAutonomia();
+	}
+	
+	
+	
+}
--- Reto4DanielMarcosHector/src/clases/Puerto.java
+++ Reto4DanielMarcosHector/src/clases/Puerto.java
@@ -3,19 +3,4 @@ package clases;
 import java.util.ArrayList;
 
 public class Puerto {
-
-	protected ArrayList<Barco> barcos;
-	
-	public boolean entrarBarco(Barco b) {
-		if(barcos.size<6) {
-			barcos.add(b);
-			return true;
-		} else {
-			System.out.println("Barco lleno.");
-			return false;
-		}
-	}
-	public boolean salirBarco(String nombre) {
-		if(nombre)
-	}
 }
